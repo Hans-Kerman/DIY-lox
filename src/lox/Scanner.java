477b905f38d79f2c1df58ed881d6d00eb4db0b5f
ButterCatWSL <62628392+Hans-Kerman@users.noreply.github.com>
2026-08-22 17:19:42 +0800

    jlox(feat)完成标识符的扫描

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -19,6 +19,28 @@ class Scanner {
         this.source = source;
     }
 
+    private static final Map<String, TokenType> keywords;
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
+
+
     private boolean isAtEnd(){
         return current >= source.length();
     }
@@ -85,6 +107,24 @@ class Scanner {
         if (current + 1 >= source.length()) return '\0';
         return source.charAt(current + 1);
     }
+
+    private boolean isAlpha(char c) {
+        return  (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
     private void scanToken() {
         char c = advance();
         switch (c) {
@@ -128,6 +168,8 @@ class Scanner {
             default:
                 if (isDigit(c)) {
                     number();
+                } else if (isAlpha(c)){
+                    identifier();
                 } else {
                     Lox.error(line, "Unexpected character.");
                 }
