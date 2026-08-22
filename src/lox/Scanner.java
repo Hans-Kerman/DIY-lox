5c2f0e206fbb7900920578f190c36fe67f475966
ButterCatWSL <62628392+Hans-Kerman@users.noreply.github.com>
2026-08-22 15:59:55 +0800

    jlox(feat)完成数字字面量的扫描

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -69,6 +69,22 @@ class Scanner {
         String value = source.substring(start + 1, current - 1);
         addToken(STRING, value);
     }
+
+    private boolean isDigit(char c){
+        return c >= '0' && c <= '9';
+    }
+    private void number() {
+        while (isDigit(peek())) advance();
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();  //消费掉'.'
+            while (isDigit(peek())) advance();
+        }
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
     private void scanToken() {
         char c = advance();
         switch (c) {
@@ -110,7 +126,11 @@ class Scanner {
                 break;
             case '"': string(); break;
             default:
-                Lox.error(line, "Unexpected character.");
+                if (isDigit(c)) {
+                    number();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
                 break;
         }
     }
