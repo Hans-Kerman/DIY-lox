28b3d1bed889075c580a3aeebc65c6ac9c0b907f
ButterCatWSL <62628392+Hans-Kerman@users.noreply.github.com>
2026-09-06 20:40:52 +0800

    jlox(feat)为ast节点类增加基础的访问者模式的方法

--- src/tool/GenerateAst.java
+++ src/tool/GenerateAst.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class GenerateAst {
     public static void main(String[] args) throws IOException {
@@ -32,14 +33,32 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
 
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
         writer.println("}");
         writer.close();
+    }
+    private static void defineVisitor(
+            PrintWriter writer, String baseName, List<String> types
+    ) {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");"
+            );
+        }
+
+        writer.println("  }");
     }
     private static void defineType(
             PrintWriter writer, String baseName,
@@ -51,9 +70,16 @@ public class GenerateAst {
         String[] fields = fieldList.split(", ");
         for (String field : fields) {
             String name = field.split(" ")[1];
-            writer.println("    this." + name + " = " + name + ";");
+            writer.println("      this." + name + " = " + name + ";");
         }
 
+        writer.println("    }");
+
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" +
+                className + baseName + "(this);");
         writer.println("    }");
 
         writer.println();
