77d5cf7db6af508ee1001c5a6a377760618daae3
ButterCatWSL <62628392+Hans-Kerman@users.noreply.github.com>
2026-09-07 16:24:58 +0800

    jlox(feat)解析器完成expression、equality规则

---
+++ src/lox/Parser.java
@@ -0,0 +1,79 @@
+package lox;
+
+import java.util.List;
+
+import static lox.TokenType.*;
+
+/**
+ * 总规则：
+ * expression     → equality ;
+ * equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+ * comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+ * term           → factor ( ( "-" | "+" ) factor )* ;
+ * factor         → unary ( ( "/" | "*" ) unary )* ;
+ * unary          → ( "!" | "-" ) unary
+ *                | primary ;
+ * primary        → NUMBER | STRING | "true" | "false" | "nil"
+ *                | "(" expression ")" ;
+ */
+
+class Parser {
+    private final List<Token> tokens;   //待解析的语法标记的队列
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    private Expr expression() {
+        return equality();
+    }
+
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();    //前一个操作数/被比较数
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+    private Token peek() {
+        return tokens.get(current);
+    }
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+    private Expr comparison() {}
+
+    private Expr term() {}
+
+    private Expr factor() {}
+
+    private Expr unary() {}
+
+    private Expr primary(){}
+}
