1dc11695d66d0331519162dff68cc73908dcecc6
nixos-itx <62628392+Hans-Kerman@users.noreply.github.com>
2026-09-17 20:35:31 +0800

    jlox(feat)完善变量的嵌套和遮蔽的运算

--- jlox/src/lox/Parser.java
+++ jlox/src/lox/Parser.java
@@ -24,7 +24,8 @@ import static lox.TokenType.*;
  * program        → declaration* EOF ;
  * declaration    → varDecl | statement ;
  *      varDecl        → "var" IDENTIFIER ( "=" expression )? ";" ;
- * statement      → exprStmt | printStmt ;
+ * statement      → exprStmt | printStmt | block ;
+ * block          → "{" declaration* "}" ;
  */
 
 class Parser {
@@ -61,6 +62,7 @@ class Parser {
 
     private Stmt statement() {
         if (match(PRINT)) return printStatement();
+        if (match(LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
     }
     private Stmt printStatement() {
@@ -72,6 +74,16 @@ class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
     }
 
     private Expr assignment() {
