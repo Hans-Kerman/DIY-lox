d86770ae2513075e6181d7291079a6b963e55b97
nixos-itx <62628392+Hans-Kerman@users.noreply.github.com>
2026-09-18 19:46:38 +0800

    jlox(chore)补充基础的可调用类型骨架

--- jlox/src/lox/Interpreter.java
+++ jlox/src/lox/Interpreter.java
@@ -1,5 +1,6 @@
 package lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
@@ -56,6 +57,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
                 return isEqual(left, right);
         }
         return null;
+    }
+
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+        Object callee = evaluate(expr.callee);
+
+        List<Object> arguments = new ArrayList<>();
+        for (Expr argument : expr.arguments) {
+            arguments.add(evaluate(argument));
+        }
+
+        LoxCallable function = (LoxCallable)callee;
+        return function.call(this, arguments);
     }
 
     @Override
