1a4cf2e7ef3c223636ed6fa1b3224d2233fc97e5
nixos-itx <62628392+Hans-Kerman@users.noreply.github.com>
2026-09-18 19:42:55 +0800

    jlox(feat)完成函数的解析

--- jlox/src/lox/Parser.java
+++ jlox/src/lox/Parser.java
@@ -18,7 +18,9 @@ import static lox.TokenType.*;
  * term           → factor ( ( "-" | "+" ) factor )* ;
  * factor         → unary ( ( "/" | "*" ) unary )* ;
  * unary          → ( "!" | "-" ) unary
- *                | primary ;
+ *                | call ;
+ * call           → primary ( "(" arguments? ")" )* ;
+ * arguments      → expression ( "," expression )* ;
  * primary        → NUMBER | STRING | "true" | "false" | "nil"
  *                | "(" expression ")"
  *                | IDENTIFIER;
@@ -79,7 +81,7 @@ class Parser {
         return expressionStatement();
     }
     private Stmt forStatement() {
-        consume(LEFT_PAREN, "Expect '(' after'for'.");
+        consume(LEFT_PAREN, "Expect '(' after 'for'.");
 
         Stmt initializer;
         if (match(SEMICOLON)) {
@@ -318,7 +320,37 @@ class Parser {
             Expr right = unary();
             return new Expr.Unary(operator, right);
         }
-        return primary();
+        return call();
+    }
+
+    private Expr finishCall(Expr callee) {
+        List<Expr> arguments = new ArrayList<>();
+        if (!check(RIGHT_PAREN)) {
+            do {
+                if (arguments.size() >= 255) {
+                    error(peek(), "Can't have more than 255 arguments.");
+                }
+                arguments.add(expression());
+            } while (match(COMMA));
+        }
+
+        Token paren = consume(RIGHT_PAREN,
+                "Expect ')' after arguments.");
+
+        return new Expr.Call(callee, paren, arguments);
+    }
+    private Expr call() {
+        Expr expr = primary();
+
+        while (true) {
+            if (match(LEFT_PAREN)) {
+                expr = finishCall(expr);
+            } else {
+                break;
+            }
+        }
+
+        return expr;
     }
 
     private Expr primary(){
