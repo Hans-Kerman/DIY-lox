0efbcc1616d5a79cc46ab0601b4f9195b6a3021c
nixos-itx <62628392+Hans-Kerman@users.noreply.github.com>
2026-09-21 21:56:52 +0800

    jlox(feat)解析变量声明

--- jlox/src/lox/Resolver.java
+++ jlox/src/lox/Resolver.java
@@ -20,6 +20,15 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         endScope();
         return null;
     }
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        declare(stmt.name);
+        if (stmt.initializer != null) {
+            resolve(stmt.initializer);
+        }
+        define(stmt.name);
+        return null;
+    }
 
     private void resolve(Stmt stmt) {
         stmt.accept(this);
@@ -38,5 +47,17 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
     private void endScope() {
         scopes.pop();
+    }
+
+    //
+    private void declare(Token name) {
+        if (scopes.isEmpty()) return;
+
+        Map<String, Boolean> scope = scopes.peek();
+        scope.put(name.lexeme, false);
+    }
+    private void define(Token name) {
+        if (scopes.isEmpty()) return;
+        scopes.peek().put(name.lexeme, true);
     }
 }
